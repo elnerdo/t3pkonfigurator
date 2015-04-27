ee857ae23303c36102c329a4aaef61955b4fe50e
Aaron Szerencses <as@imko.de>
2015-04-27 12:36:54 +0200

    Removes a lot of trash from imagebuilder.

--- imagebuilder.py
+++ imagebuilder.py
@@ -10,34 +10,6 @@ class ImageBuilder(object):
         self.image_spacer80 = '<img class="img160" width="20px"src="/t3pkonfigurator/static/spacer80.png">'
         self.canvas_actions = ''
 
-    def build(self):
-        return "<script>$('#background').ready(function(){" + self.canvas_actions + "});</script>"
-
-    def set_background(self):
-        self.canvas_actions += "set_background();"
-
-    def set_tube(self, tube):
-        self.canvas_actions += "set_tubehead();set_tube({0});".format(tube)
-
-    def set_elements(self, elements, tube, depths):
-        self.canvas_actions += "set_elements({0}, {1}, {2});".format(elements, tube, depths)
-
-    def build_canvas(self, configuration):
-        self.configuration = configuration
-        imagedict = {
-            'probe': ['16', '40'],
-            'spacer10': ['16', '20'],
-            'spacer30': ['16', '60'],
-            'spacer80': ['16', '160']
-        }
-
-        #js = self.set_background()
-        js = self.draw_tube()
-
-        return js
-
-
-    def draw_element(self, index, id, x, y, width, height):
-        js = 'var img{index} = document.getElementById("img-{id}");\
-ctx.drawImage(img{index}, {x}, {y}, {width}, {height});'
-        return js.format(index=index, id=id, x=x, y=y, width=width, height=height)
+    def build(self, elements, tube, depths):
+        variables = "draw_canvas({0}, {1}, {2});".format(elements, tube, depths)
+        return "<script>$('#background, #tubeimg-100, #tubeimg-top, #tubeimg-bottom').ready(function(){" + variables + "});</script>"
