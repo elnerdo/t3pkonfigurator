6b6f1a1be093838aaa66540678a4ea17be5e9559
Aaron Szerencses <as@imko.de>
2015-04-29 14:02:05 +0200

    Removes unused files.

--- static/js/debugg.js
+++
@@ -1,7 +0,0 @@
-function set_resolution(w, h) {
-    $('body').height(h);
-    $('body').width(w);
-    var content = "width=" + w + ", height=" + h + ", initial-scale=1.0";
-    $('#viewport').attr('content', content);
-    //$('#container').css({width: w, height: h});
-}
